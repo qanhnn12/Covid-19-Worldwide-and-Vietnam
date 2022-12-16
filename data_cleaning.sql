f14420373679767990fbbd27df875da4832f9923
Anh Nguyen <84619797+qanhnn12@users.noreply.github.com>
2022-12-16 18:50:05 +0700

    Update data_cleaning.sql

--- data_cleaning.sql
+++ data_cleaning.sql
@@ -1,5 +1,6 @@
+--Table "cases"
 ALTER TABLE cases
-ALTER COLUMN total_cases INT null;
+ALTER COLUMN total_cases INT null,
 
 ALTER TABLE cases
 ALTER COLUMN new_cases INT null;
@@ -10,6 +11,7 @@ ALTER COLUMN total_deaths INT null;
 ALTER TABLE cases
 ALTER COLUMN new_deaths INT null;
 
+--Table "tests"
 ALTER TABLE tests
 ALTER COLUMN total_tests BIGINT null;
 
@@ -22,6 +24,7 @@ ALTER COLUMN positive_rate FLOAT null;
 ALTER TABLE tests
 ALTER COLUMN tests_per_case FLOAT null;
 
+--Table "vaccinations"
 ALTER TABLE vaccinations
 ALTER COLUMN total_vaccinations BIGINT null;
 
@@ -36,3 +39,16 @@ ALTER COLUMN total_boosters BIGINT null;
 
 ALTER TABLE vaccinations
 ALTER COLUMN new_vaccinations BIGINT null;
+
+----Table "hospitals"
+ALTER TABLE hospitals
+ALTER COLUMN icu_patients INT null;
+
+ALTER TABLE hospitals
+ALTER COLUMN hosp_patients INT null;
+
+ALTER TABLE hospitals
+ALTER COLUMN weekly_icu_admissions INT null;
+
+ALTER TABLE hospitals
+ALTER COLUMN weekly_hosp_admissions INT null;
